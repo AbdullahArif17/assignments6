1b862d2c82c5a520fb864b23862359b065208050
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-11-25 23:56:13 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Main from "@/components/Main/page";
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Nav/>
-      <Main/>
+      <Header />
+      <Nav />
+      <Main />
     </div>
   );
 }
