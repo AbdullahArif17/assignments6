f202a907abb5b30a8b14601acc70f064603f84aa
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-11-25 23:38:44 +0500

    figma design

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Header from "@/components/Header/page";
+import Header from "@/components/Header/page"; 
 import Nav from "@/components/Nav/page";
 import Main from "@/components/Main/page";
 
-export default function () {
+export default function Home() {
   return (
     <div>
       <Header/>
-      <Nav />
+      <Nav/>
       <Main/>
     </div>
   );
