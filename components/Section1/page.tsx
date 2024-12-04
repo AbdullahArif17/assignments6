f94511f5bf30520ea70824d1b19c5099a7b4ffcd
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-12-05 00:00:04 +0500

    update

--- components/Section1/page.tsx
+++ components/Section1/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function SecOne() {
   return (
-    <div className="flex flex-col justify-center items-center h-auto w-full py-6">
-      <div className="m-4 w-[327px] text-left">
+    <div className="flex flex-col justify-center items-center md:flex-row md:gap-16 md:h-[228px] md:bg-[#F7F7F7] h-auto w-full py-6">
+      <div className="m-4 w-[327px] md:m-16 text-left">
         <p className="font-roboto text-black text-[18px] font-bold leading-[27px]">
-          Trusted by the world's best companies 
-          <span > [Social proof to build credibility]</span>
+          Trusted by the world's best companies [Social proof to build
+          credibility]
         </p>
       </div>
 
